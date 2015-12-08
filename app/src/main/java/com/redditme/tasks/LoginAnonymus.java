d421721ef116e99b1d13001593b8fca4b53d9371
Ferenc Hammerl <ferenchammerl@outlook.com>
2015-12-08 13:40:20 +0100

    Authentication is done with userlessApp now.

--- app/src/main/java/com/redditme/tasks/LoginAnonymus.java
+++ app/src/main/java/com/redditme/tasks/LoginAnonymus.java
@@ -10,7 +10,7 @@ import java.util.UUID;
  * Created by Ferenc on 12/5/2015.
  */
 public interface LoginAnonymus {
-    public static final Credentials USERLESS = Credentials.userless("M5E32UMDH6Dmvw", "RdNUf_ARr300FPiZuY9zVLXZ-oc", UUID.randomUUID());
+    public static final Credentials USERLESS = Credentials.userlessApp("hsro81jAMJgXhw", UUID.randomUUID());
     public static final String VERSION_NAME =  "0.1";
     public static final String PACKAGE_NAME =  "com.redditme";
     public RedditClient getAuthenticatedRedditClient() throws OAuthException;
--- app/src/test/java/com/redditme/AuthorizationTest.java
+++ app/src/test/java/com/redditme/AuthorizationTest.java
@@ -1,8 +1,11 @@
 package com.redditme;
 
 import net.dean.jraw.RedditClient;
+import net.dean.jraw.models.CommentNode;
+import net.dean.jraw.models.Submission;
 import net.dean.jraw.models.Subreddit;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static junit.framework.Assert.assertNotNull;
@@ -11,9 +14,15 @@ import static junit.framework.Assert.assertNotNull;
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class AuthorizationTest {
+    RedditClient redditClient;
+
+    @Before
+    public void setUp() throws Exception {
+        redditClient = new MockLoginAnonymus().getAuthenticatedRedditClient();
+    }
+
     @Test
     public void testAnonRandomSubreddit() throws Exception {
-        RedditClient redditClient = new MockLoginAnonymus().getAuthenticatedRedditClient();
         Subreddit sub = redditClient.getRandomSubreddit();
         assertNotNull(sub);
         System.out.println(sub.getDisplayName());
@@ -26,5 +35,16 @@ public class AuthorizationTest {
 //        System.out.println("Top comment body text: " + cmt.getComment().getBody());
 //
 //        System.out.println(cnode.getDepth());
+    }
+
+    @Test
+    public void testCommentFromSubmission() throws Exception {
+        Submission subm = redditClient.getSubmission("3viu31");
+        System.out.println("Submission title: " + subm.getTitle());
+        System.out.println("Submission number of comments: " + subm.getCommentCount());
+        CommentNode cnode = subm.getComments();
+        CommentNode cmt = cnode.get(0);
+        System.out.println("Author of top comment: " + cmt.getComment().getAuthor());
+        System.out.println("Top comment: " + cmt.getComment().getBody());
     }
 }
