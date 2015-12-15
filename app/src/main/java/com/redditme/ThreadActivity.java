142fee181ab7fdc8dc73e2e985386c8619c0e404
Johannes Normann Jensen <jnj@iportalen.dk>
2015-12-15 19:05:04 +0100

    image view popup

--- app/src/main/java/com/redditme/ThreadActivity.java
+++ app/src/main/java/com/redditme/ThreadActivity.java
@@ -1,8 +1,13 @@
 package com.redditme;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -11,6 +16,8 @@ import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -26,6 +33,10 @@ import com.redditme.utils.ThumbnailGenerator;
 
 import net.dean.jraw.models.Submission;
 
+import java.io.IOException;
+import java.net.URI;
+import java.util.Iterator;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -73,15 +84,59 @@ public class ThreadActivity extends AppCompatActivity {
         content.setLayoutManager(llm);
 
     }
+
+    Drawable drawable_from_url(String url, String src_name) throws
+            java.net.MalformedURLException, java.io.IOException {
+        return Drawable.createFromStream((java.io.InputStream)
+                new java.net.URL(url).getContent(), src_name);
+    }
+
     @Override
     public void startActivity(Intent intent) {
         if (TextUtils.equals(intent.getAction(), Intent.ACTION_VIEW)) {
             System.out.println("clicked");
-        }
-        else {
+            Drawable dr = null;
+            try {
+                thumbnailGenerator.execute(intent.getDataString()).get(12, TimeUnit.SECONDS);
+                dr = thumbnailGenerator.gimmeTheThumbnail();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            } catch (TimeoutException e) {
+                e.printStackTrace();
+            } finally {
+                if (dr == null) {
+                    startActivity(intent, null);
+                    return;
+                }
+            }
+
+            AlertDialog.Builder imageDialog = new AlertDialog.Builder(this);
+            LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+
+            View layout = inflater.inflate(R.layout.full_image,
+                    (ViewGroup) findViewById(R.id.image_root));
+            ImageView image = (ImageView) layout.findViewById(R.id.fullimageView);
+            image.setImageDrawable(dr);
+            imageDialog.setView(layout);
+            imageDialog.setPositiveButton("sheet", new DialogInterface.OnClickListener(){
+
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+
+            });
+
+
+            imageDialog.create();
+            imageDialog.show();
+
+        } else {
             System.out.println("not clicked");
         }
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -101,7 +156,7 @@ public class ThreadActivity extends AppCompatActivity {
             return true;
         }
 
-        if(id == R.id.action_refresh){
+        if (id == R.id.action_refresh) {
 //            Here we need to refresh the current subreddit
             Toast.makeText(getApplicationContext(), "Refreshing", Toast.LENGTH_SHORT).show();
             return true;
